d071827bbae7f105c2862c1f3af8c626243f4a4e
Matt Brown <matt.brown@mailtrust.com>
2013-12-19 18:03:03 -0500

    Update things to work with the github API.

---
+++ GithubHooks/Helpers/LowercaseContractResolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json.Serialization;
+
+namespace GithubHooks.Helpers
+{
+    public class LowercaseContractResolver : DefaultContractResolver
+    {
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            return propertyName.ToLower();
+        }
+    }
+}
---
+++ GithubHooks/Models/MergeResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Octokit;
+
+namespace GithubHooks.Models
+{
+    public class MergeResult
+    {
+        [JsonProperty("sha")]
+        public string Sha;
+        [JsonProperty("merged")]
+        public bool Merged;
+        [JsonProperty("message")]
+        public string Message;
+    }
+}
